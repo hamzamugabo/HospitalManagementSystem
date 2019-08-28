fccf79c4173e385ae0b0978d3ba8d51fd5352939
hamza mugabo <hamzamugabo3@gmail.com>
2019-08-28 14:02:47 +0300

    first commit

---
+++ src/hospitalmanagementapp2/HospitalManagementApp2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospitalmanagementapp2;
+
+/**
+ *
+ * @author hamza
+ */
+public class HospitalManagementApp2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
